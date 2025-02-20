8a27567cc936d49c51f641fa1e2b109fd71e0599
sient <sient@GIGIDIDTY>
2025-02-20 15:02:02 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UhhUhhhUhhHhuHhhIdk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
